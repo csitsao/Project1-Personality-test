2b331ab894d442c44a58e6108891d2e7544309a1
csitsao <csitsao@users.noreply.github.com>
2017-09-03 17:12:55 -0700

    Input/Output finished
    Opening/Reading files and Exporting/Closing files

--- PJ1.py
+++ PJ1.py
@@ -1,3 +1,6 @@
+def sortPercentage(line):
+    return line
+
 print """
 This program processes a file of answers to the Keirsey Temperament Sorter.
 It converts the various A and B answers for each person into a sequence of
@@ -8,15 +11,26 @@ Please enter the name of your file that you would like to process:
 
 filename = raw_input("> ")
 inputfile = open(filename, "r")
+outputfile = open("output.txt", "w")
 
-# inputfile.readlines() # This turn all the lines into a list
+"""
+The output will be stored in: output.txt
+"""
+
+# print inputfile.readlines() # This turn all the lines into a list
 
 names = []
 results = []
 
 for line in inputfile:
     if len(line) < 70:
-        names.append(line)
+        outputfile.write(line.rstrip('\r\n')) # .rstrip() removes characters from the end (right side)
+        outputfile.write(": ")
+    else:
+        percentage = sortPercentage(line)
+        outputfile.write(percentage)
+
+
 
 
 
@@ -27,3 +41,4 @@ for line in inputfile:
 
 
 inputfile.close()
+outputfile.close()
